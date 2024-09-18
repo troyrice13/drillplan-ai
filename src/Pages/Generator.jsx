60ebc50c079de94408602f681177a692f9b7e7c0
Troy Rice <troyanthonyrice@gmail.com>
2024-09-18 13:17:03 -0700

    messages

--- src/Pages/Generator.jsx
+++ src/Pages/Generator.jsx
@@ -3,15 +3,36 @@ import './Generator.css'
 
 export default function Generator() {
     const [input, setInput] = useState('');
+    const [messages, setMessages] = useState([
+        {sender: 'user', text:'hello'}
+    ])
 
     const handleChange = (e) => {
         setInput(e.target.value)
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (input.trim()) {
+            setMessages([...messages, {sender: 'user', text: input}])
+        }
+        
+        setInput('')
+    }
+
     return (
         <div className="generator-container">
-            <form>
+                <ul className="chatbox">
+                    {messages.map((message, index) => (
+                        <li key={index} className="message.sender">
+                            {message.text}
+                        </li>
+                    ))}
+                </ul>
+            <form onSubmit={handleSubmit}>
                 <input type="text" value={input} onChange={handleChange} className="input-box" />
-                    <button>Go</button>
+                    <button type="submit">Go</button>
             </form>
         </div>
     )
