0e6699c72cf33e5fd411f54915bde3a494f896c3
Troy Rice <troyanthonyrice@gmail.com>
2024-09-19 15:11:11 -0700

    login formatted

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,16 +3,21 @@ import './Login.css'
 
 export default function Login() {
     return (
-        <>
-            <div className="login-container">
-                <div className="login">
-                    <h1 className="login-title">Sign In</h1>
-                    <form className="login-form">
-                        <input className="username" type='text' placeholder="Username" />
-                        <input className="password" type='text' placeholder="Password" />
-                    </form>
-                </div>
+    <>
+        <div className="login-container">
+            <div className="login">
+                <h1 className="login-title">Sign In</h1>
+                <form className="login-form">
+                    <input className="username" type='text' placeholder="Username" />
+                    <input className="password" type='text' placeholder="Password" />
+                    <span className="btn-container">
+                        <button className="forgot-password">Forgot Password</button>
+                        <button className="signup-btn">Signup</button>
+                    </span>
+                    <button className="login-btn">Login</button>
+                </form>
             </div>
+        </div>
         </>
     )
 }
