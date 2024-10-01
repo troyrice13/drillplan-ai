cf9e3e77c82226a23a9d1c623d8b569c7e19512d
Troy Rice <troyanthonyrice@gmail.com>
2024-10-01 14:06:42 -0700

    favicon

--- backend/models/User.js
+++ backend/models/User.js
@@ -41,6 +41,12 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
     email: {
         type: String,
         required: true,
